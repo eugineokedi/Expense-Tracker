6b23047ac61b5f7b320dc173b22576ad9164943f
alchemist1111 <emoyoeugine@gmail.com>
2024-05-17 20:25:42 +0300

    Added components and content

---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const IncomeExpenses = () => {
+  return (
+    <div className='inc-exp-container'>
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" className='moneyPlus'>+$0.00</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" className='moneyMinus'>-$0.00</p>
+      </div>
+    </div>
+  )
+}
+
+export default IncomeExpenses
